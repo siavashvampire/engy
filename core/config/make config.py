0ec2602e7528de63fb370f133604b686a5d7af3e
siavash_pc <sepahisiavash@gmail.com>
2022-11-12 13:47:57 +0330

    add petit bot for test

--- core/config/make config.py
+++ core/config/make config.py
@@ -6,6 +6,9 @@ db = TinyDB('config.json')
 db.drop_tables()
 table = db.table('config')
 
-table.insert({'telegram_token': "5712479914:AAHzQldpI9MBVl8jzcvl6m2wN3todG7rn8U"})
+if dev:
+    table.insert({'telegram_token': "5377461148:AAFekpap_Fs-C-_3CVPi50nexYOsAQK-IuQ"})
+else:
+    table.insert({'telegram_token': "5712479914:AAHzQldpI9MBVl8jzcvl6m2wN3todG7rn8U"})
 table.update({'admin_id': 99981475})
 
